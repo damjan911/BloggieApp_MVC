40e3e092f55576a653e54649596fe679b7dd75c0
Damjan <119892938+damjan911@users.noreply.github.com>
2024-01-01 16:00:06 +0100

    Update IBlogPostRepository.cs

--- Bloggie.Web/Repositories/Interfaces/IBlogPostRepository.cs
+++ Bloggie.Web/Repositories/Interfaces/IBlogPostRepository.cs
@@ -5,6 +5,7 @@ namespace Bloggie.Web.Repositories.Interfaces
 	public interface IBlogPostRepository
 	{
 		Task<BlogPost?> GetByIdAsync(Guid id);
+		
 		Task<IEnumerable<BlogPost>> GetAllBlogsAsync();
 
 		Task<BlogPost> CreateAsync(BlogPost blogPost);
